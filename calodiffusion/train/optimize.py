a67828d439c8e33536fe4995eea7979b932acbf0
voetberg <magpie127@gmail.com>
2025-10-06 06:48:20 -0700

    debug

--- calodiffusion/train/optimize.py
+++ calodiffusion/train/optimize.py
@@ -10,6 +10,7 @@ import os
 from datetime import datetime
 from calodiffusion.utils import utils
 from calodiffusion.train import evaluate
+from calodiffusion.train.train import Train
 
 class Objective(ABC): 
     @staticmethod
